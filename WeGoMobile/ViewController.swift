473c35b18776467d0c4abe0471d0280ba5379a5c
Colby Tang <colby.us2012@gmail.com>
2020-04-29 19:17:21 -0700

    Can make a successful POST request, need to fix the alert error.

--- WeGoMobile/ViewController.swift
+++ WeGoMobile/ViewController.swift
@@ -12,9 +12,78 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
+    // hiding the keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func btn_SignIn(_ sender: UIButton) {
+        let params = ["username":txt_Username.text, "password":txt_Password.text]
+        POSTRequest(server: "demand", endpoint: "api/cs/login", params: params)
+    }
+    
+    @IBOutlet weak var txt_Username: UITextField!
+    @IBOutlet weak var txt_Password: UITextField!
+    
+    func POSTRequest (server: String, endpoint: String, params: Dictionary<String, String?>) {
+        print ("server: \(server) endpoint: \(endpoint) params: \(params)")
+        var request = URLRequest(url: URL(string: "https://\(server).team12.softwareengineeringii.com/\(endpoint)")!)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) {
+            (data, response, error) in
+            guard error == nil else {
+                print("error calling POST on /request/1")
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
 
+        // parse the result as JSON, since that's what the API provides
+        do {
+            guard let serverResponse = try JSONSerialization.jsonObject(with: responseData,
+                options: []) as? [String: Any] else {
+                    print("Could not get JSON from responseData as dictionary")
+                    return
+            }
+            print("The server response is: " + serverResponse.description)
+            guard let first_name = serverResponse["first_name"] as? String else {
+                print("Could not get first_name as String from JSON")
+                return
+            }
+            print("The First Name is: \(first_name)")
+        } catch  {
+            if let stringResponse = String(data: data!, encoding: String.Encoding.utf8) as String? {
+                print (stringResponse)
+                let alertController = UIAlertController(title: "Response from Server", message: "FAILED", preferredStyle: UIAlertController.Style.alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+            else {
+                print("error parsing response from POST on /serverResponse")
+            }
+            return
+        }
+      }
+
+        task.resume()
+    }
+    
+    func createAlert (title: String, message: String) {
+        print ("Create Alert: Title: \(title) Message: \(message)")
+
+    }
 }
 
